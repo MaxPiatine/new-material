bdb2fc3d80cdb29a18b164aa127776197c755985
MaxPiatine <max.piatine@hotmail.com>
2023-04-23 19:17:34 -0500

    hackerrank control-sequence problem 1

--- ruby/hello-world.rb
+++ ruby/hello-world.rb
@@ -18,3 +18,8 @@ end
 
 # array manipulation 
 # https://www.freecodecamp.org/news/common-array-methods-in-ruby/
+
+def rot13(secret_messages)
+    # your code here
+      secret_messages.map {|x| x.tr("a-z", "n-za-m")}
+  end
